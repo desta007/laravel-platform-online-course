6c35366ee7797ee7715f3fa3e86a333c3b1b4afa
Desta <detajuventini@gmail.com>
2024-10-29 21:25:14 +0700

    feat: crud category, add teacher

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -29,14 +31,10 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'icon' => ['required', 'image', 'mimes:png,jpg,jpeg']
-        ]);
-
-        DB::transaction(function () use ($validated, $request) {
+        DB::transaction(function () use ($request) {
+            $validated = $request->validated();
             if ($request->hasFile('icon')) {
                 $iconPath = $request->file('icon')->store('icons', 'public');
                 $validated['icon'] = $iconPath;
@@ -64,15 +62,26 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        DB::transaction(function () use ($request, $category) {
+            $validated = $request->validated();
+            if ($request->hasFile('icon')) {
+                $iconPath = $request->file('icon')->store('icons', 'public');
+                $validated['icon'] = $iconPath;
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+
+            $category->update($validated);
+        });
+        return redirect()->route('admin.categories.index');
     }
 
     /**
@@ -80,6 +89,16 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $category->delete();
+            DB::commit();
+
+            return redirect()->route('admin.categories.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('admin.categories.index')->with('error', 'terjadi sebuah error');
+        }
     }
 }
