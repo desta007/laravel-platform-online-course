2b3f944d62f134ee420e60888f1dc3d3b463268a
Desta <detajuventini@gmail.com>
2024-10-30 22:26:30 +0700

    feat: add and delete courses

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCourseRequest;
+use App\Models\Category;
 use App\Models\Course;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CourseController extends Controller
 {
@@ -12,7 +18,18 @@ class CourseController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+        $query = Course::with(['category', 'teacher', 'students'])->orderByDesc('id');
+
+        if ($user->hasRole('teacher')) {
+            $query->whereHas('teacher', function ($query) use ($user) {
+                $query->where('user_id', $user->id);
+            });
+        }
+
+        $courses = $query->paginate(10);
+
+        return view('admin.courses.index', compact('courses'));
     }
 
     /**
@@ -20,15 +37,43 @@ class CourseController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('admin.courses.create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCourseRequest $request)
     {
-        //
+        $teacher = Teacher::where('user_id', Auth::user()->id)->first();
+        if (!$teacher) {
+            return redirect()->route('admin.courses.index')->withErrors('Unauthorized or invalid teacher.');
+        }
+
+        DB::transaction(function () use ($request, $teacher) {
+            $validated = $request->validated();
+
+            if ($request->hasFile('thumbnail')) {
+                $thumbnailPath = $request->file('thumbnail')->store('thumbnail', 'public');
+                $validated['thumbnail'] = $thumbnailPath;
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+            $validated['teacher_id'] = $teacher->id;
+
+            $course = Course::create($validated);
+
+            if (!empty($validated['course_keypoints'])) {
+                foreach ($validated['course_keypoints'] as $keyPointText) {
+                    $course->course_keypoints()->create([
+                        'name' => $keyPointText
+                    ]);
+                }
+            }
+        });
+
+        return redirect()->route('admin.courses.index');
     }
 
     /**
@@ -60,6 +105,19 @@ class CourseController extends Controller
      */
     public function destroy(Course $course)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $course->delete();
+            $course->course_keypoints()->delete();
+            $course->course_videos()->delete();
+
+            DB::commit();
+
+            return redirect()->route('admin.courses.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('admin.courses.index')->with('error', 'terjadi sebuah error');
+        }
     }
 }
